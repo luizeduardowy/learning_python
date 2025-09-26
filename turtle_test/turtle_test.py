f874b9339913f1dc46bd587a0b310316d1780a80
Luiz Eduardo Wanderley <luizeduardowy@gmail.com>
2025-09-26 22:04:49 +0100

    turtle

--- turtle_test/turtle_test.py
+++ turtle_test/turtle_test.py
@@ -6,19 +6,27 @@ t.pensize(2)
 t.speed(5)
 
 def move_t_up():
+    t.speed(10)
     t.setheading(90)
+    t.speed(5)
     t.forward(25)
 
 def move_t_down():
+    t.speed(10)
     t.setheading(270)
+    t.speed(5)
     t.forward(25)
 
 def move_t_right():
+    t.speed(10)
     t.setheading(0)
+    t.speed(5)
     t.forward(25)
 
 def move_t_left():
+    t.speed(10)
     t.setheading(180)
+    t.speed(5)
     t.forward(25)
 
 screen.onkey(move_t_down, 'Down')
@@ -26,5 +34,6 @@ screen.onkey(move_t_up, 'Up')
 screen.onkey(move_t_left, 'Left')
 screen.onkey(move_t_right, 'Right')
 
+screen.delay(25)
 screen.listen()
 screen.mainloop()
